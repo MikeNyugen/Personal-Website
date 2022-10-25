de7f9271077fdeb0060b6e245f0126cbe331f45b
Mike Nguyen <mikenguyen017@gmail.com>
2022-10-25 16:57:27 +0100

    Added tooltips for socials

--- src/components/Socials/Socials.js
+++ src/components/Socials/Socials.js
@@ -1,19 +1,95 @@
 import "./Socials.css";
+import Tooltip from "@mui/material/Tooltip";
+import Zoom from "@mui/material/Zoom";
 
 const Socials = () => {
   return (
     <div className="social-div">
-      <a href="https://github.com/MikeNyugen" target="_blank">
-        <img className="icon" src={require("./github.png")} />
-      </a>
+      <Tooltip
+        title="GitHub repository"
+        placement="bottom"
+        TransitionComponent={Zoom}
+        TransitionProps={{ timeout: 150 }}
+        arrow
+        componentsProps={{
+          tooltip: {
+            sx: {
+              fontSize: "25px",
+              backgroundColor: "white",
+              "& .MuiTooltip-arrow": {
+                color: "white",
+                width: "0px",
+              },
+              color: "black",
+              fontFamily: "helvetica",
+            },
+          },
+        }}
+      >
+        <a href="https://github.com/MikeNyugen" target="_blank">
+          <img
+            className="icon"
+            src={require("./github.png")}
+            alt="GitHub icon"
+          />
+        </a>
+      </Tooltip>
 
-      <a href="https://www.linkedin.com/in/mike-n-644b901b3/" target="_blank">
-        <img className="icon" src={require("./linkedin.png")} />
-      </a>
+      <Tooltip
+        title="Linkedin Profile"
+        placement="bottom"
+        TransitionComponent={Zoom}
+        TransitionProps={{ timeout: 150 }}
+        arrow
+        componentsProps={{
+          tooltip: {
+            sx: {
+              fontSize: "25px",
+              backgroundColor: "white",
+              "& .MuiTooltip-arrow": {
+                color: "white",
+                width: "0px",
+              },
+              color: "black",
+              fontFamily: "helvetica",
+            },
+          },
+        }}
+      >
+        <a href="https://www.linkedin.com/in/mike-n-644b901b3/" target="_blank">
+          <img
+            className="icon"
+            src={require("./linkedin.png")}
+            alt="LinkedIn icon"
+          />
+        </a>
+      </Tooltip>
 
-      <a href="https://www.linkedin.com/in/mike-n-644b901b3/" target="_blank">
-        <img className="icon" src={require("./mail.png")} />
-      </a>
+      <Tooltip
+        title="Send me an email"
+        placement="bottom"
+        TransitionComponent={Zoom}
+        TransitionProps={{ timeout: 150 }}
+        arrow
+        componentsProps={{
+          tooltip: {
+            sx: {
+              fontSize: "25px",
+              backgroundColor: "white",
+              "& .MuiTooltip-arrow": {
+                color: "white",
+                width: "0px",
+              },
+              color: "black",
+              fontFamily: "helvetica",
+            },
+          },
+        }}
+      >
+        <a href="https://www.linkedin.com/in/mike-n-644b901b3/" target="_blank">
+          <img className="icon" src={require("./mail.png")} alt="mail icon" />
+        </a>
+      </Tooltip>
     </div>
   );
 };
